7822cbdc6ad11c4610bcff8168a1cbbcdc4b53f4
Eduardo Azevedo <eduardoazevedo3@gmail.com>
2024-09-03 23:21:39 -0300

    refactor: boleto processor

--- src/boleto/processors/boleto.processor.ts
+++ src/boleto/processors/boleto.processor.ts
@@ -1,9 +1,10 @@
 import { OnWorkerEvent, Processor, WorkerHost } from '@nestjs/bullmq';
 import { Job } from 'bullmq';
+import { Boleto } from '../entities/boleto.entity';
 
 @Processor('boleto')
 export class BoletoProcessor extends WorkerHost {
-  async process(job: Job<any, any, string>): Promise<any> {
+  async process(job: Job<{ boletoId: number }, Boleto, string>): Promise<void> {
     console.log(
       `Processing boleto job "${job.id}" of type "${job.name}" with data ${JSON.stringify(job.data)}`,
     );
