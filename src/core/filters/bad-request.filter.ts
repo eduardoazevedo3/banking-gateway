c09ecbffc77e026c6a05ce30a100e9f5209c38e8
Eduardo Azevedo <eduardoazevedo3@gmail.com>
2024-09-12 23:09:59 -0300

    fix: bad request filter

--- src/core/filters/bad-request.filter.ts
+++ src/core/filters/bad-request.filter.ts
@@ -1,9 +1,8 @@
 import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
 import { EntityNotFoundError } from 'typeorm/error/EntityNotFoundError';
-import { AuthBadRequestException } from '../../banking/exceptions/auth-bad-request.exception';
 import { RecordValidationException } from '../../boleto/exceptions/record-validation.exception';
 
-@Catch(RecordValidationException, AuthBadRequestException)
+@Catch(RecordValidationException)
 export class BadRequestFilter implements ExceptionFilter {
   catch(e: EntityNotFoundError, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
