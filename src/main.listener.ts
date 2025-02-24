ac75478a4e64f800c2d020c6dbeea5437db47d1f
Eduardo Azevedo <eduardoazevedo3@gmail.com>
2025-02-23 21:29:10 -0300

    fix: docker-compose.yml

--- src/main.listener.ts
+++ src/main.listener.ts
@@ -1,8 +1,16 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppListenerModule } from './app.listener.module';
 
 async function bootstrap() {
+  const port = process.env.LISTENER_PORT || 3000;
   const app = await NestFactory.create(AppListenerModule);
-  await app.listen(3001);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  await app.listen(port);
 }
 bootstrap();
